4accf71719eb4877c2e4ccb07f7305ec30b49415
hariharandhulipala <153804236+hariharandhulipala@users.noreply.github.com>
2023-12-13 23:57:05 -0800

    Update views.py - Added edit option and email checks.
    Added the edit option

--- contact-20231214T074657Z-001/contact/views.py
+++ contact-20231214T074657Z-001/contact/views.py
@@ -20,3 +20,15 @@ def upload(request):
             # return HttpResponse("""your form is wrong, reload on <a href = "{{ url : 'index'}}">reload</a>""")
     else:
         return render(request, 'contact/upload_form.html', {'upload_form':upload})
+
+def update_contact(request, contact_id):
+    contact_id = int(contact_id)
+    try:
+        contact_sel = Contact.objects.get(id = contact_id)
+    except Contact.DoesNotExist:
+        return redirect('index')
+    contact_form = ContactCreate(request.POST or None, instance = contact_sel)
+    if contact_form.is_valid():
+       contact_form.save()
+       return redirect('index')
+    return render(request, 'contact/upload_form.html', {'upload_form':contact_form})
