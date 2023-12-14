4ee2adb1cdcc99a45be042209497fe854faf239d
hariharandhulipala <153804236+hariharandhulipala@users.noreply.github.com>
2023-12-13 23:55:55 -0800

    Update views.py

--- contact-20231214T074657Z-001/contact/views.py
+++ contact-20231214T074657Z-001/contact/views.py
@@ -8,3 +8,15 @@ def index(request):
     shelf = Contact.objects.all()
     return render(request, 'contact/contactsapp.html', {'shelf': shelf})
 
+def upload(request):
+    upload = ContactCreate()
+    if request.method == 'POST':
+        upload = ContactCreate(request.POST, request.FILES)
+        if upload.is_valid():
+            upload.save()
+            return redirect('index')
+        else:
+            return render(request, 'contact/popup.html')
+            # return HttpResponse("""your form is wrong, reload on <a href = "{{ url : 'index'}}">reload</a>""")
+    else:
+        return render(request, 'contact/upload_form.html', {'upload_form':upload})
